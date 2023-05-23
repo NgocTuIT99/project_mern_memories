e97f252e49fb942345b53a8ecdc5b674cf15b101
NgocTuIT99 <ngoctuvo493@gmail.com>
2023-05-23 13:35:43 +0700

    vercel deploy

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://memories-project:memories-project123@cluster0.qtliw.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://memories-project:memories-project123@cluster0.qtliw.mongodb.net/';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
